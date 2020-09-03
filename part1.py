6a92a5179f35fdec4ff4fb1704842ae9f25febd7
Lorenzo <lorhernandez@csumb.edu>
2020-09-02 22:45:05 -0700

    first commit

---
+++ part1.py
@@ -0,0 +1,37 @@
+db = [
+    {
+        "id": "A",
+        "item": "soda",
+        "cost": 1.00,
+        "volume_discount": {
+            "qty": 4,
+            "cost": 3.00
+        }
+    },
+    {
+        "id": "B",
+        "item": "apple",
+        "cost": 0.45,
+        "volume_discount": {
+            "qty": 3,
+            "cost": 1.00
+        }
+    },
+    {
+        "id": "C",
+        "item": "jelly",
+        "cost": 3.00,
+        "volume_discount": None
+    },
+    {
+        "id": "D",
+        "item": "kleenex",
+        "cost": 6.00,
+        "volume_discount": None
+    },
+]
+
+
+receipt_1 = "ABCD"
+receipt_2 = "DCCBAABB"
+
